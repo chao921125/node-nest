297c7442271a8e1e064db0113ee5ea0f6aec352a
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-10-30 16:29:58 +0800

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+// like this java entity or bean
 @Module({
   imports: [],
   controllers: [AppController],
